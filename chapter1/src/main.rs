e137a6a3eec5e46cd6ace59de57d533ec9b13b26
keyem4251 <kisk.emura@gmail.com>
2022-05-11 21:43:57 +0900

    段落を分ける

--- chapter1/src/main.rs
+++ chapter1/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    fn_1_1();
+    fn_1_2();
+}
+
+fn fn_1_1() {
+    // 切れ目がはっきりしないコード
+    let quantity = 10;
+    let unit_price = 2;
+    let mut _price = quantity * unit_price;
+    if _price < 3000 {
+        _price += 500;
+    }
+    _price = _price * 30;
+}
+
+fn fn_1_2() {
+    // 空白行を入れて3つの段落に分けたコード
+    let quantity = 10;
+    let unit_price = 2;
+    let mut _price = quantity * unit_price;
+
+    if _price < 3000 {
+        _price += 500;
+    }
+
+    _price = _price * 30;
 }
