5ff2616ef42fd5c5979849eed1c4d3faabb8e67a
keyem4251 <kisk.emura@gmail.com>
2022-05-12 11:26:43 +0900

    クラスに分割

--- chapter1/src/main.rs
+++ chapter1/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     fn_1_2();
     fn_1_3();
     fn_1_4();
+    fn_1_5();
 }
 
 fn fn_1_1() {
@@ -59,4 +60,40 @@ fn shipping_cost(base_price: i32) -> i32 {
         return 500;
     }
     0
+}
+
+struct ShippingCost {
+    base_price: i32,
+}
+
+impl ShippingCost {
+    const MINIMUM_FOR_FREE: i32 = 3000;
+    const COST: i32 = 500;
+
+    fn new(base_price: i32) -> Self {
+        Self { base_price }
+    }
+
+    fn amount(&self) -> i32 {
+        if self.base_price < Self::MINIMUM_FOR_FREE {
+            return Self::COST
+        }
+        0
+    }
+}
+
+fn shipping_cost2(base_price: i32) -> i32 {
+    let cost = ShippingCost::new(base_price);
+    cost.amount()
+}
+
+fn fn_1_5() {
+    // 異なるクラスの重複したコードをなくす
+    let quantity = 10;
+    let unit_price = 2;
+    let base_price = quantity * unit_price;
+
+    let shipping_cost = shipping_cost2(base_price);
+
+    let _item_price = (base_price + shipping_cost) * 30;
 }
