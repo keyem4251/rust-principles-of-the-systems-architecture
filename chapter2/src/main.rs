f97b01d2d56bfd85be0d0aa4bff640f714f0891d
keyem4251 <kisk.emura@gmail.com>
2022-05-14 16:54:11 +0900

    メソッドに抽出

--- chapter2/src/main.rs
+++ chapter2/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    fn_2_1();
+}
+
+fn fn_2_1() {
+    // メソッドに抽出する
+    fn is_child(cutomer_type: &str) -> bool {
+        cutomer_type == "child"
+    }
+
+    fn child_fee(base_fee: f32) -> f32 {
+        base_fee * 0.5
+    }
+
+    if is_child("child") {
+        let _fee = child_fee(10.0);
+    }
 }
