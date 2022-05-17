146a3ddefbc98df8c07c87d188700dd76aac5f42
keyem4251 <kisk.emura@gmail.com>
2022-05-17 21:15:50 +0900

    クラスに分割、インスタンス変数を使用する

--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
-    println!("Hello, world!");
+    fn_3_1();
+}
+
+fn fn_3_1() {
+    // メソッドはインスタンス変数を使い、クラスが肥大化したら小さく分ける
+    struct Customer {
+        person_name: PersonName,
+    }
+
+    impl Customer {
+        fn new(person_name: PersonName) -> Self {
+            Customer { person_name }
+        }
+    }
+
+    struct PersonName {
+        first_name: String,
+        last_name: String,
+    }
+
+    impl PersonName {
+        fn new(first_name: String, last_name: String) -> Self {
+            PersonName {
+                first_name,
+                last_name,
+            }
+        }
+
+        fn full_name(&self) -> String {
+            format!("{} {}", self.first_name, self.last_name)
+        }
+    }
+
+    let person_name = PersonName::new("first_name".to_string(), "last_name".to_string());
+    let customer = Customer::new(person_name);
+    println!("{}", customer.person_name.full_name());
 }
