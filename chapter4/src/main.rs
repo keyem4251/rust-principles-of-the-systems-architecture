6150408f005d26487d289a202f5edfa771c04624
keyem4251 <kisk.emura@gmail.com>
2022-05-19 21:45:21 +0900

    業務ルールを扱うクラス

--- chapter4/src/main.rs
+++ chapter4/src/main.rs
@@ -1,3 +1,25 @@
+use chrono::{Date, Local};
+
 fn main() {
-    println!("Hello, world!");
+    fn_4_1();
+}
+
+struct DueDate {
+    due_date: Date<Local>
+}
+
+impl DueDate {
+    fn new(due_date: Date<Local>) -> Self {
+        DueDate { due_date }
+    }
+
+    fn is_expired(&self, date: &Date<Local>) -> bool {
+        date > &self.due_date
+    }
+}
+
+fn fn_4_1() {
+    // 期日の業務ルールを扱うクラス
+    let due_date = DueDate::new(Local::today());
+    println!("{}", due_date.is_expired(&Local::today()));
 }
