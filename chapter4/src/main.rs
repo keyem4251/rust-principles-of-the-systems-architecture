ea16d6bab1b923a091ec3c5c753842727171f9b7
keyem4251 <kisk.emura@gmail.com>
2022-05-20 22:57:25 +0900

    add rule

--- chapter4/src/main.rs
+++ chapter4/src/main.rs
@@ -1,4 +1,6 @@
 use chrono::{Date, Local};
+use std::collections::HashSet;
+use std::hash::Hash;
 
 fn main() {
     fn_4_1();
@@ -22,4 +24,35 @@ fn fn_4_1() {
     // 期日の業務ルールを扱うクラス
     let due_date = DueDate::new(Local::today());
     println!("{}", due_date.is_expired(&Local::today()));
+}
+
+trait Rule<T> where T: Eq + Hash + PartialEq {
+    fn ok(&self, value: i32) -> bool;
+    fn ng(&self, value: i32) -> bool {
+        self.ok(value)
+    }
+}
+
+struct Policy<T> {
+    rules: HashSet<Box<dyn Rule<T>>>
+}
+
+impl<T> Policy<T> {
+    fn new() -> Self {
+        let rules: HashSet<Box<dyn Rule<T>>> = HashSet::new();
+        Policy { rules }
+    }
+
+    fn comply_with_all(&self, value: i32) -> bool {
+        for rule in &self.rules {
+            if rule.ng(value) {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn add_rule(&mut self, rule: &Box<dyn Rule<T>>) {
+        self.rules.insert(rule);
+    }
 }
